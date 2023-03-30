3b6c70a6012f30d3c670db4018d00554e1d5711c
nifanism <128988266+nifanism@users.noreply.github.com>
2023-03-30 13:05:25 +0800

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -78,7 +78,6 @@ IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
   heading: PropTypes.string,
-  subheading: PropTypes.string,
   mainpitch: PropTypes.object,
   description: PropTypes.string,
   intro: PropTypes.shape({
