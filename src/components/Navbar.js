2a19587c147204af3289007e53bac7b6b748c1ab
nifanism <128988266+nifanism@users.noreply.github.com>
2023-03-31 09:37:44 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../img/ergonomic-logo.png";
+import logo from "../img/arvintengislogo.png";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
