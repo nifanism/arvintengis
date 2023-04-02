b3939214e40e9e3643b8d5f0a651114268ecbd06
nifanism <128988266+nifanism@users.noreply.github.com>
2023-04-02 16:03:33 +0800

    Update product-page.js

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -69,22 +69,6 @@ export const ProductPageTemplate = ({
                     </div>
                   </div>
                 </div>
-                <Testimonials testimonials={testimonials} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="section section--gradient">
-        <div className="container">
-          <div className="section">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <h2 className="has-text-weight-semibold is-size-2">
-                  {pricing.heading}
-                </h2>
-                <p className="is-size-5">{pricing.description}</p>
-                <Pricing data={pricing.plans} />
               </div>
             </div>
           </div>
