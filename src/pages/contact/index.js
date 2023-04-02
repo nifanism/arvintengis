db9f1706adf76ef9c53917ec0257db34665ad774
nifanism <128988266+nifanism@users.noreply.github.com>
2023-04-02 16:56:18 +0800

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -40,72 +40,11 @@ export default class Index extends React.Component {
           <div className="container">
             <div className="content">
               <h1>Холбоо барих</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
                 <div className="field">
                   <label className="label" htmlFor={"name"}>
                     Таны нэр
                   </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"text"}
-                      name={"name"}
-                      onChange={this.handleChange}
-                      id={"name"}
-                      required={true}
-                    />
-                  </div>
                 </div>
-                <div className="field">
-                  <label className="label" htmlFor={"email"}>
-                    Цахим хаяг
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"email"}
-                      name={"email"}
-                      onChange={this.handleChange}
-                      id={"email"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"message"}>
-                   Холбоо барих 
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={"message"}
-                      onChange={this.handleChange}
-                      id={"message"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Илгээх
-                  </button>
-                </div>
-              </form>
             </div>
           </div>
         </section>
