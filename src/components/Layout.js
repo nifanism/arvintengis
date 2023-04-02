9390ecf91d0f52294ad1d9ca504ace8798478620
nifanism <128988266+nifanism@users.noreply.github.com>
2023-04-02 19:30:20 +0800

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix("/")}img/erconomics.jpg`}
+          content={`${withPrefix("/")}img/ergonomics.jpg`}
         />
       </Helmet>
       <Navbar />
