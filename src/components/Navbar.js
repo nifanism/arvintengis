4c482d0527436f8eb3a4047eb7d9d09202199d60
nifanism <128988266+nifanism@users.noreply.github.com>
2023-04-03 13:56:13 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,11 +44,6 @@ const Navbar = () => {
             </Link>
             </li>
             <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/blog">
-              Блог
-            </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/contact">
               Холбоо барих
             </Link>
